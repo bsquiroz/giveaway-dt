af52c402428d590c05f9879d40ff0cbafe5e54d6
bsquiroz <bemq1605@gmail.com>
2024-03-18 00:41:28 -0500

    delete env url

--- src/api/giveaway.api.ts
+++ src/api/giveaway.api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL_BACKEND =
-    import.meta.env.VITE_BASE_URL_API ||
-    "https://sorteos-app-backend.andrescode.com/api/v1";
+const BASE_URL_BACKEND = "https://sorteos-app-backend.andrescode.com/api/v1";
 
 const giveawayApi = (token?: string) => {
     if (!token)
