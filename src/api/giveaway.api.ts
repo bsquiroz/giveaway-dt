ce173289f51ce1859fd7e2b9143702648bb129f3
bsquiroz <bemq1605@gmail.com>
2024-03-17 20:23:25 -0500

    change url

--- src/api/giveaway.api.ts
+++ src/api/giveaway.api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL_BACKEND = import.meta.env.VITE_BASE_URL_API;
+// const BASE_URL_BACKEND = import.meta.env.VITE_BASE_URL_API;
+const BASE_URL_BACKEND = "sorteos-app-backend.andrescode.com/api/v1";
 
 const giveawayApi = axios.create({
     baseURL: BASE_URL_BACKEND,
